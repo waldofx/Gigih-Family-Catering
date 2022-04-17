4ca65434625c1a6b8f57be4c70a68beda38a75e8
waldofx <90760767+waldofx@users.noreply.github.com>
2022-04-17 16:00:11 +0700

    Food model validation

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,18 @@
 class Food < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :description, presence: true, length: {
+        maximum: 150,
+        message: "must be less than 150 character"
+    }
+    validates :price, numericality: {
+        only_float: true,
+        message: "must be numeric"
+    }, comparison: {
+        greater_than_or_equal_to: 0.01,
+        message: "must be greater than or equal to 0.01"
+    }
+
+    def self.by_letter(letter)
+        where("name LIKE ?", "#{letter}%").order(:name)
+    end
 end
--- spec/factories/foods.rb
+++ spec/factories/foods.rb
@@ -1,7 +1,22 @@
 FactoryBot.define do
   factory :food do
-    name { "MyString" }
-    description { "MyText" }
-    price { 1.5 }
+    name { "Nasi Uduk" }
+    description { "Betawi style steamed rice cooked in coconut milk. Delicious!" }
+    price { 10000.0 }
+  end
+
+  factory :invalid_food, parent: :food do
+    name { nil }
+    description { nil }
+    price { 10000.0 }
   end
 end
+
+# faker
+# FactoryBot.define do
+#   factory :food do
+#     name { Faker::Food.dish }
+#     description { Faker::Food.description }
+#     price { 10000.0 }
+#   end
+# end
