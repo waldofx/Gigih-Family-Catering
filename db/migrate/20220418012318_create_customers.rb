366357113ed842b79f9be3f8b9849d4a86dc6f1c
waldofx <90760767+waldofx@users.noreply.github.com>
2022-04-18 09:10:25 +0700

    Customer phone int to string

--- db/migrate/20220418012318_create_customers.rb
+++ db/migrate/20220418012318_create_customers.rb
@@ -2,7 +2,7 @@ class CreateCustomers < ActiveRecord::Migration[7.0]
   def change
     create_table :customers do |t|
       t.string :name
-      t.integer :phone
+      t.string :phone
       t.string :address
       t.string :email
 
