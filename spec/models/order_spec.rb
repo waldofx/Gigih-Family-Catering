86353401c70dfb1cd8059aef69cc826f5ea07619
waldofx <90760767+waldofx@users.noreply.github.com>
2022-04-20 10:00:39 +0700

    Order and orderdetails total validation specs

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -23,4 +23,18 @@ RSpec.describe Order, type: :model do
   it "has many orderdetails" do
     should respond_to(:orderdetails)
   end
+
+  it 'should correctly count total from all orderdetails' do  
+    Customer.create(name: 'Xilef', phone: "+6281234567888", address: 'Jakarta', email:'waldofelix3@gmail.com')
+    Order.create(customer_id: 1, order_date: '2022-04-18', status: 'NEW')
+    Menuitem.create(name: 'Nasi Gempreng', description: 'Nasi Uduk is nice', price: 10000.0)
+    Menuitem.create(name: 'Nasi Jombrong', description: 'Nasi Jombrong is nice', price: 5000.0)
+
+    Orderdetail.create(order_id: 1, menuitem_id: 1, quantity: 2)
+    Orderdetail.create(order_id: 1, menuitem_id: 2, quantity: 1)
+
+    order1 = Order.find(1)
+
+    expect(order1[:total]).to eq(25000.0)
+  end
 end
--- spec/models/orderdetail_spec.rb
+++ spec/models/orderdetail_spec.rb
@@ -2,10 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Orderdetail, type: :model do
   it "has a valid factory" do
+    Menuitem.create(name: 'Nasi Gempreng', description: 'Nasi Uduk is nice', price: 10000.0)
+    Customer.create(name: 'Xilef', phone: "+6281234567888", address: 'Jakarta', email:'waldofelix2@gmail.com')
+    Order.create(customer_id: 1, order_date: '2022-04-18', status: 'NEW')
     expect(FactoryBot.build(:orderdetail)).to be_valid
   end
   
   it 'is valid with an order_id, menuitem_id, and quantity' do
+    Menuitem.create(name: 'Nasi Gempreng', description: 'Nasi Uduk is nice', price: 10000.0)
+    Customer.create(name: 'Xilef', phone: "+6281234567888", address: 'Jakarta', email:'waldofelix2@gmail.com')
+    Order.create(customer_id: 1, order_date: '2022-04-18', status: 'NEW')
     expect(FactoryBot.build(:orderdetail)).to be_valid
   end
 
@@ -35,5 +41,17 @@ RSpec.describe Orderdetail, type: :model do
   it "belongs to menuitem" do
     t = Orderdetail.reflect_on_association(:menuitem)
     expect(t.macro).to eq(:belongs_to)
+  end
+
+  it 'should correctly count total from item_price * quantity' do  
+    Customer.create(name: 'Xilef', phone: "+6281234567888", address: 'Jakarta', email:'waldofelix3@gmail.com')
+    Order.create(customer_id: 1, order_date: '2022-04-18', status: 'NEW')
+    Menuitem.create(name: 'Nasi Gempreng', description: 'Nasi Uduk is nice', price: 10000.0)
+
+    Orderdetail.create(order_id: 1, menuitem_id: 1, quantity: 2)
+
+    orderdetail1 = Orderdetail.find(1)
+    
+    expect(orderdetail1[:total]).to eq(20000.0)
   end
 end
