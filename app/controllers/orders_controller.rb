93edca75c2db6e471e52e90fa5c7b95922601eeb
waldofx <90760767+waldofx@users.noreply.github.com>
2022-04-21 20:27:44 +0700

    More details on GET especially orders/:id

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -61,6 +61,8 @@ class OrdersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_order
       @order = Order.find(params[:id])
+      @customer = Customer.find(@order[:customer_id])
+      @orderdetails = Orderdetail.where("order_id = ?", @order[:id])
     end
 
     # Only allow a list of trusted parameters through.
