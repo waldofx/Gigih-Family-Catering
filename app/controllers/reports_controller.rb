a21ea25110dd30fd17ec3cd92da5c5a0ce4caa56
waldofx <90760767+waldofx@users.noreply.github.com>
2022-04-23 00:19:08 +0700

    Update views

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,11 +1,8 @@
 class ReportsController < ApplicationController
-  before_action :set_report, only: %i[ show edit update destroy ]
-
   # GET /reports or /reports.json
   def index
     today = DateTime.current.to_date
     @reports = Order.joins(:customer).where("order_date = ?", today)
-    # @reports = Order.joins("INNER JOIN menuitems ON menuitems.id = orderdetails.menuitem_id")
   end
   
 end
